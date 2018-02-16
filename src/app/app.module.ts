a9077c8347fd9f1536b2c925419590d9ff9d5434
PavelNET <pavlo.netrebsky@gmail.com>
2018-02-16 19:05:01 +0200

    Delete comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-// import { NgRedux, NgReduxModule } from 'ng2-redux';
 import { StoreModule } from '@ngrx/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
 import { NewCounterComponent } from './new-counter/new-counter.component';
@@ -18,15 +17,9 @@ import { CountersListComponent } from './counters-list/counters-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    // NgReduxModule,
-    // StoreModule.provideStore({'count': rootReducer})
     StoreModule.forRoot({'counters': rootReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  // constructor(ngRedux: NgRedux<IAppState>) {
-  //   ngRedux.configureStore(rootReducer, INITIAL_STATE);
-  // }
-}
+export class AppModule {}
