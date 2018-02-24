77aabeb68b1cb2582059f73b8136089279fecaa0
PavelNET <pavlo.netrebsky@gmail.com>
2018-02-24 15:07:54 +0200

    New module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { StoreModule } from '@ngrx/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
 import { NewCounterComponent } from './new-counter/new-counter.component';
