f3724894e868b51767c985248aebe1549d1bc746
PavelNET <pavlo.netrebsky@gmail.com>
2018-02-25 13:06:47 +0200

    Package store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
-import { StoreModule } from '@ngrx/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
 import { NewCounterComponent } from './new-counter/new-counter.component';
 import { CountersListComponent } from './counters-list/counters-list.component';
